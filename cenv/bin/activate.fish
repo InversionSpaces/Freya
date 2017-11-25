adb6a2a94602756d91069882ff3e0ef652d13939
InversionSpaces <InversionSpaces@vivaldi.net>
2017-11-25 19:02:58 +0300

    Almost get json from server

--- cenv/bin/activate.fish
+++ cenv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/inv/Prjs/CanSite/cenv"
+set -gx VIRTUAL_ENV "/home/inv/Site/GRMSite/cenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
---
+++ run.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+    
+app = Flask(__name__)
+
+@app.route("/ol/ol.js")
+def ol():
+    with open("ol/ol.js") as f:
+        return f.read()
+
+@app.route("/")
+def index():
+    return render_template("map.html")
+
+@app.route("/data")
+def data():
+    with open("geoex.json") as f:
+        return f.read()
+
+app.run()
